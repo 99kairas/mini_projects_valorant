2e0d1fbf78d93beb8e883831ef5fc0bc35fc409c
Rizki Andika Setiadi <106527864+99kairas@users.noreply.github.com>
2023-05-07 17:16:07 +0700

    Menambahkan Fitur Agent Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mini_projects_valorant/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
+      home: HomeScreen(),
     );
   }
 }
